27a066273a6585c02bd025fbdaddf41ed297acdb
kharismajd <kharismajagaddhita170501@gmail.com>
2020-11-23 08:31:32 +0700

    unfinished battle.pl

--- battle.pl
+++ battle.pl
@@ -1,4 +1,4 @@
 :- dynamic(isFighting/1).
-:- dynamic(trigerredEnemy/8). /** Musuh yang sedang dilawan. trigerredEnemy(Name, Level, MaxHP, HP, Attack, Defense, Gold, EXP) **/
+:- dynamic(trigerredEnemy/8). /** Musuh yang sedang dilawan. trigerredEnemy(Name, Level, MaxHP, HP, Attack, Defense, Gold_Drop, EXP_Drop) **/
 
-
+/** Under construction **/
