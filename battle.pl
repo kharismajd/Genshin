b555c3d9b1ec08de3199c719fd67597daa9a9ad1
Rais Vaza <raisvaza@gmail.com>
2020-11-23 15:38:01 +0700

    adding battle mechanism

--- battle.pl
+++ battle.pl
@@ -1,5 +1,6 @@
 :- dynamic(isFighting/1).
 :- dynamic(trigerredEnemy/8). /** Musuh yang sedang dilawan. trigerredEnemy(Name, Level, MaxHP, HP, Attack, Defense, Gold_Drop, EXP_Drop) **/
+:- dynamic(playerStatus/8).
 
 /** Under construction **/
 foundEnemy :- 
@@ -17,4 +18,10 @@ foundEnemy :-
     write('What will you do?').
 
 attack :-
-    
+    EnemyAttack <= PlayerDefense,
+    PlayerAttack <= EnemyDefense,
+    trigerredEnemy(Name, EnemyLevel, EnemyMaxHP, EnemyHP, EnemyAttack, EnemyDefense, Gold_Drop, EXP_Drop),
+    playerStatus(PlayerLevel, PlayerClass, PlayerMaxHP, PlayerHP, PlayerAttack, PlayerDefense, PlayerGold, PlayerEXP).
+
+attack :-
+    EnemyAttack > PlayerDefense.
