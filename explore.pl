1c106c67f262b06f8593d0626b7381ced308f4e5
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2020-11-26 22:55:18 +0700

    Perbaikan fungsi map

--- explore.pl
+++ explore.pl
@@ -1,28 +1,41 @@
 :- dynamic(koord/2). /** koord(X, Y) **/
-:- dynamic(isValid/2). /** isValid(X, Y) **/
+:- dynamic(action/0).
 
 :- public(w/0).
 :- public(a/0).
 :- public(s/0).
 :- public(d/0).
 
+:- include('map.pl').
+
 w :- koord(X,Y),
 	X_ is X-1,
 	isValid(X_,Y), !,
 	retractall(koord(_,_)),
-	asserta(koord(X_,Y)).
+	asserta(koord(X_,Y)),
+    action.
 s :- koord(X,Y),
 	X_ is X+1,
 	isValid(X_,Y), !,
 	retractall(koord(_,_)),
-	asserta(koord(X_,Y)).
+	asserta(koord(X_,Y)),
+    action.
 a :- koord(X,Y),
 	Y_ is Y-1,
 	isValid(X,Y_), !,
 	retractall(koord(_,_)),
-	asserta(koord(X,Y_)).
+	asserta(koord(X,Y_)),
+    action.
 d :- koord(X,Y),
 	Y_ is Y+1,
 	isValid(X,Y_), !,
 	retractall(koord(_,_)),
-	asserta(koord(X,Y_)).
+	asserta(koord(X,Y_)),
+    action.
+
+isValid(X,Y) :-
+    X >= 0,
+    Y >= 0,
+    map_size(Xm,Ym),
+    X < Xm,
+    Y < Ym.
--- map.pl
+++ map.pl
@@ -1,5 +1,7 @@
 :- dynamic(special_lokasi/3).
 
+:- public(map_size/2).
+:- public(map/0).
 
 map_size(10,10).
 
@@ -20,130 +22,25 @@ special_lokasi(10,10, 'D').
 special_lokasi(5, 4, 'Q').
 special_lokasi(2, 2, 'S').
 
-/* X = Lebar , Y = Tinggi */
-/*Membuat MAP */
+/* Menulis satu koordinat */
 
-/*Batas Kanan*/
+print_koord(0,_) :- !, write('#').
+print_koord(_,0) :- !, write('#').
+print_koord(X,_) :- map_size(Xm,_), X =:= Xm+2, !, write('#').
+print_koord(_,Y) :- map_size(_,Ym), Y =:= Ym+2, !, write('#').
 
-printMAP(X,Y) :- 
-        map_size(T,L),
-                X =:= T+1,
-                Y =< L+1,
-                write('#'),n1,
-                Z  is Y+1
-                printMAP(0,Z).
+print_koord(X,Y) :- special_lokasi(X,Y,Z), !, write(Z).
 
-/*Batas Kiri */
+print_koord(X,Y) :-
+    X > 0,
+    Y > 0,
+    map_size(Xm,Ym),
+    X =< Xm+1,
+    Y =< Ym+1,
+    !, write(' ').
 
-printMAP(X,Y) :-
-        map_size(_,L),
-                X =:= T+1,
-                Y =< L+1,
-                write('#'),
-                Z is X+1,
-                printMAP(Z,Y).
-
-/*Batas  Atas */
-
-printMAP(X,Y) :-
-        map_size(T,_),
-                X < T + 1,
-                X > 0,
-                Y =:= 0,
-                write('#'),
-                Z is X+1,
-                printMAP(Z,Y).
-
-/*Batas Bawah */
-
-printMAP(X,Y) :-
-        map_size(T,L),
-                X < T + 1,
-                X > 0,
-                Y =:= L+1,
-                write('#'),
-                Z is X+1,
-                printMAP(Z,Y).
-        
-/*Lokasi Special*/
-printMAP(X, Y) :- 
-        map_size(T, L),
-		X < T + 1,
-		X > 0,
-		Y < L + 1,
-		Y > 0,
-		special_lokasi(X, Y, 'P'),
-		special_lokasi(X, Y, 'Q'), !,
-		write('P'),
-		write(' '),
-		Z is X+1,
-		printMAP(Z, Y).
-
-/*Lokasi Special*/
-printMAP(X, Y) :- 
-        map_size(T, L),
-		X < T + 1,
-		X > 0,
-		Y < L + 1,
-		Y > 0,
-		special_lokasi(X, Y, 'D'),
-		write('D'),
-		write(' '),
-		Z is X+1,
-		printMAP(Z, Y).
-
-/*Lokasi Special*/
-printMAP(X, Y) :- 
-        map_size(T, L),
-		X < T + 1,
-		X > 0,
-		Y < L + 1,
-		Y > 0,
-		special_lokasi(X, Y, 'S'),
-		write('S'),
-		write(' '),
-		Z is X+1,
-		printMAP(Z, Y).
-
-/*Lokasi Special*/
-printMAP(X, Y) :- 
-        map_size(T, L),
-		X < T + 1,
-		X > 0,
-		Y < L + 1,
-		Y > 0,
-		special_lokasi(X, Y, 'Q'),
-		write('Q'),
-		write(' '),
-		Z is X+1,
-		printMAP(Z, Y).
-
-/*Lokasi Special*/
-printMAP(X, Y) :- 
-        map_size(T, L),
-		X < W + 1,
-		X > 0,
-		Y < H + 1,
-		Y > 0,
-		special_lokasi(X, Y, Obj), !,
-		write(Obj),
-		write(' '),
-		Z is X+1,
-		printMAP(Z, Y).
-
-/*Lokasi Special*/
-/* Empty */
-printMAP(X, Y) :- 
-        map_size(T, L),
-		X < W + 1,
-		X > 0,
-		Y < H + 1,
-		Y > 0,
-		(\+ special_lokasi(X, Y, _)),
-		write('-'),
-		Z is X+1,
-		printMAP(Z, Y).
-
-
-
-
+map :-
+    map_size(Xm_,Ym_),
+    Xm is Xm_+2,
+    Ym is Ym_+2,
+    forall(between(0,Ym,Y),(forall(between(0,Xm,X),print_koord(X,Y)),nl)).
