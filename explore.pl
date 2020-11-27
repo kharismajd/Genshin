fd5a5d874166863b7c1d0924718043f41e7b0ea8
Rais Vaza <raisvaza@gmail.com>
2020-11-27 14:26:36 +0700

    perbaikan navigasi wasd

--- explore.pl
+++ explore.pl
@@ -7,31 +7,31 @@
 :- public(d/0).
 
 w :- koord(X,Y),
-    X_ is X-1,
-    isValid(X_,Y), !,
-    retractall(koord(_,_)),
-    asserta(koord(X_,Y)),
-    (action; true).
-
-s :- koord(X,Y),
-    X_ is X+1,
-    isValid(X_,Y), !,
-    retractall(koord(_,_)),
-    asserta(koord(X_,Y)),
-    (action; true).
-
-a :- koord(X,Y),
     Y_ is Y-1,
     isValid(X,Y_), !,
     retractall(koord(_,_)),
     asserta(koord(X,Y_)),
     (action; true).
 	
-d :- koord(X,Y),
+s :- koord(X,Y),
     Y_ is Y+1,
     isValid(X,Y_), !,
     retractall(koord(_,_)),
     asserta(koord(X,Y_)),
+    (action; true).
+
+a :- koord(X,Y),
+    X_ is X-1,
+    isValid(X_,Y), !,
+    retractall(koord(_,_)),
+    asserta(koord(X_,Y)),
+    (action; true).
+
+d :- koord(X,Y),
+	X_ is X+1,
+    isValid(X_,Y), !,
+    retractall(koord(_,_)),
+    asserta(koord(X_,Y)),
     (action; true).
 
 isValid(_,_) :-
